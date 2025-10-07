d74284e622773c9922a4a6963238d29c2582fa69
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-10-07 01:45:16 +0000

    orm

---
+++ user-api/src/main/java/com/example/demo/repository/IslandRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import com.example.demo.repository.entity.Island;
+
+public interface IslandRepository 
+    extends ListCrudRepository<Island, Long> {
+    
+}
